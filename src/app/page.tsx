2617f71b4de1f6f70dd95e152db3eb9e7e9dcc90
jrajni <jrajni6@gmail.com>
2025-06-19 16:59:38 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   if (session?.user) {
     void api.post.getLatest.prefetch();
   }
-
+  console.log("first");
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
